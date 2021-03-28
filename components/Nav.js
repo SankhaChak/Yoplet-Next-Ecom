b1311ae2c7c2ad97fcf7b39db0771a252e301b0d
SankhaChak <anonproductionspro@gmail.com>
2021-03-28 12:55:55 +0530

    🏗 component: Navbar

---
+++ components/Nav.js
@@ -0,0 +1,17 @@
+import { FaShoppingCart } from "react-icons/fa";
+import styles from "./Nav.module.css";
+
+const Nav = () => {
+  return (
+    <nav className={styles.nav}>
+      <p className={styles.navTitle}>Yoplet</p>
+      <p className={styles.navCart}>
+        <button>
+          <FaShoppingCart /> $0.00
+        </button>
+      </p>
+    </nav>
+  );
+};
+
+export default Nav;
